eb21314eda1d50733ef4cd27332f50baa58f44f7
BlackNinja <zz188@duke.edu>
2021-12-29 10:57:44 +0800

    fixed download by file bugs

--- download.py
+++ download.py
@@ -12,8 +12,6 @@ def parse_args():
     return parser.parse_args()
 
 def metadata(filt):
-    page = 1
-    page_num = 1
     filt_path = list()
     filt_url = list()
     print("Retrieving metadata...")
@@ -23,31 +21,36 @@ def metadata(filt):
         filt_url.append(f.replace(' ', '%20'))
         filt_path.append((f.replace(' ', '')).replace(':', '_').replace("\"",""))
 
-    path = 'dataset/metadata/' + ''.join(filt_path)
+#     path = 'dataset/metadata/' + ''.join(filt_path)
+    paths = ['dataset/metadata/' + pa for pa in filt_path]
 
     # Overwrite metadata query folder 
-    if not os.path.exists(path):
-        os.makedirs(path)
+    for path in paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     # Save all pages of the JSON response    
-    while page < page_num + 1:
-        url = 'https://www.xeno-canto.org/api/2/recordings?query={0}&page={1}'.format('%20'.join(filt_url), page)
-        print(url)
-        try:
-            r = request.urlopen(url)
-        except error.HTTPError as e:
-            print('An error has occurred: ' + str(e))
-            exit()
-        print("Downloading metadate page " + str(page) + "...")
-        data = json.loads(r.read().decode('UTF-8'))
-        filename = path + '/page' + str(page) + '.json'
-        with open(filename, 'w') as saved:
-            json.dump(data, saved)
-        page_num = data['numPages']
-        page += 1
+    for fu in filt_url:
+        path = 'dataset/metadata/' + fu.replace('%20','')
+        page, page_num = 1, 1
+        while page < page_num + 1:
+            url = 'https://www.xeno-canto.org/api/2/recordings?query={0}&page={1}'.format(fu, page)
+            print(url)
+            try:
+                r = request.urlopen(url)
+            except error.HTTPError as e:
+                print('An error has occurred: ' + str(e))
+                exit()
+            print("Downloading metadate page " + str(page) + "...")
+            data = json.loads(r.read().decode('UTF-8'))
+            filename = path + '/page' + str(page) + '.json'
+            with open(filename, 'w') as saved:
+                json.dump(data, saved)
+            page_num = data['numPages']
+            page += 1
 
     # Return the path to the folder containing downloaded metadata
-    return path
+    return paths
 
 def listdir_nohidden(path):
     for f in os.listdir(path):
@@ -55,12 +58,12 @@ def listdir_nohidden(path):
             yield f
 
 def download(filt):
-    page = 1
-    page_num = 1
+    
+    all_recordings = []
     print("Downloading all recordings for query...")
 
     # Retrieve metadata to parse for download links
-    path = metadata(filt)
+    paths = metadata(filt)
 
     # Enumerate list of metadata folders
     path_list = listdir_nohidden("dataset/metadata/")
@@ -78,30 +81,39 @@ def download(filt):
             if line not in redown:
                 redown.add(line)
             curr.close()
+    
+    for pa in paths:
+        page = 1
+        with open(pa + '/page' + str(page) + ".json", 'r') as jsonfile:
+            data = json.loads(jsonfile.read())
+            
+        # init audio directory and collect data
+        if int(data['numRecordings']) > 0:
+            bird_name = data['recordings'][0]['en'].replace(' ','')
+            audio_dir = 'dataset/audio/' + bird_name
+            if not os.path.exists(audio_dir):
+                os.makedirs(audio_dir)
+        all_recordings += data['recordings']
+        page_num = data['numPages']
 
-    with open(path + '/page' + str(page) + ".json", 'r') as jsonfile:
-        data = jsonfile.read()
-    data = json.loads(data)
-    page_num = data['numPages']
-    
-    # combine multiple pages for multiprocessing
-    if page_num > 1:
-        for i in range(2, page_num+1):
-            with open(path + '/page' + str(i) + ".json", 'r') as jsonfile:
-                new_data = json.loads(jsonfile.read())
-                data['recordings'] += new_data['recordings']
+        # combine multiple pages for multiprocessing
+        if page_num > 1:
+            for i in range(2, page_num+1):
+                with open(pa + '/page' + str(i) + ".json", 'r') as jsonfile:
+                    new_data = json.loads(jsonfile.read())
+                    all_recordings += new_data['recordings']
                 
-    print("Found " + str(data['numRecordings']) + " recordings for given query, downloading...") 
+    print("Found " + str(len(all_recordings)) + " recordings for given queries, downloading...") 
     
-    for i in tqdm(range(len(data['recordings'])), desc="downloading"):
-        url = data['recordings'][i]['file']
+    for curr_rec in tqdm(all_recordings, desc="downloading"):
+        url = curr_rec['file']
         # redirect url and update real url
         re_url = requests.get(url, allow_redirects=True).url
-        name = (data['recordings'][i]['en']).replace(' ', '')
-        track_id = data['recordings'][i]['id']
+        name = (curr_rec['en']).replace(' ', '')
+        track_id = curr_rec['id']
 
         # Keep track of the most recently downloaded file
-        recent = open(path + "/in_progress.txt", "w")
+        recent = open('dataset/audio/' + name + "/in_progress.txt", "w")
         recent.write(str(track_id))
         recent.write("\n")
         recent.close()
@@ -126,7 +138,7 @@ def download(filt):
         request.urlretrieve(url, audio_path + audio_file)
 
         # If the method has completed successfully, then we can delete the in_progress file
-        os.remove(path + "/in_progress.txt")
+        os.remove('dataset/audio/' + name + "/in_progress.txt")
 
 if __name__ == '__main__':
     
@@ -135,6 +147,7 @@ if __name__ == '__main__':
     if isFile:
         with open(args.name, 'r') as f:
             all_birds = [n.lower() for n in f.read().split('\n') if n]
+            print(all_birds)
     else:
         all_birds = [args.name.lower()]
     download(all_birds)
