7217321bee66468ca51342024528c24f7c81b348
ziang <zz188@duke.edu>
2022-05-05 17:01:33 +0800

    optimzed multiprocess task assignment

--- utils.py
+++ utils.py
@@ -1,8 +1,18 @@
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
+    to_return = []
     portion = len(lst) // n 
     for i in range(0, n):
         if i == (n - 1):
-            yield lst[portion*i:]
+            last_port = lst[portion*i:]
+            diff = len(last_port) - portion
+            
+            for j in range(diff):
+                to_return[j].append(last_port[j])
+                
+            last_port = last_port[diff:]
+            to_return.append(last_port)
+            
         else:
-            yield lst[portion*i: portion*(i+1)]
+            to_return.append(lst[portion*i: portion*(i+1)])
+    return to_return
